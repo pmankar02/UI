c366948565c2a2b9e03d005a2937710f583b7ad5
pmankar02 <pmankar02@gmail.com>
2025-03-06 20:15:34 +0530

    newchanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
+
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Ui';
+  title = 'UI';
 }
---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-header',
+  standalone: false,
+  templateUrl: './page-header.component.html',
+  styleUrl: './page-header.component.scss'
+})
+export class PageHeaderComponent {
+
+}
