a6573cda5303a2ea5cd338951d21c959a3818b70
pmankar02 <pmankar02@gmail.com>
2025-03-20 19:31:30 +0530

    new changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(fb: FormBuilder){
+    this.loginForm = fb.group({
+      email: fb.control ('', [Validators.required]),
+      password: fb.control ('',[Validators.required]),
+   });
+
+  }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ApiService } from '../../shared/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -9,17 +11,38 @@ import { FormBuilder, FormGroup} from '@angular/forms';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  hidepwdcontext: boolean = true;
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private apiService: ApiService, private snackbar: MatSnackBar) {
     this.registerForm = fb.group({
-      firstName: fb.control(''),
-      lastName: fb.control(''),
-      email: fb.control(''),
-      mobileNumber: fb.control(''),
-      password: fb.control(''),
-      rpassword: fb.control(''),
+      firstName: fb.control('',[Validators.required]),
+      lastName: fb.control('',[Validators.required]),
+      email: fb.control('',[Validators.required]),
+      mobileNumber: fb.control('',[Validators.required]),
+      password: fb.control('',[Validators.required]),
+      rpassword: fb.control('',[Validators.required]),
     });
+  }
+
+  register() {
+    let user = {
+      firstName: this.registerForm.get("firstName")?.value,
+      lastName: this.registerForm.get("lastName")?.value,
+      email: this.registerForm.get("email")?.value,
+      mobileNumber: this.registerForm.get("mobileNumber")?.value,
+      password: this.registerForm.get("password")?.value,
+    };
+
+    this.apiService.register(user).subscribe({
+      next: res => {
+
+        this.snackbar.open(res,"OK");
+      },
+
+    });
+      
+    
   }
 
 }
